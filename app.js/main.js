77e2fb4ce3da635a7738a85ce0db8e408ff472ac
zeddwwweb <abzedd@MacBook-Air-de-ab.local>
2023-02-17 22:23:16 +0100

    header 80% complet

--- app.js/main.js
+++ app.js/main.js
@@ -1,20 +1,23 @@
 //   set variable
 
-let items = document.querySelectorAll('.item')
-
-
-
-
-
-
+let items = document.querySelectorAll(".item");
 
 //  set function
-items.forEach( function(ele) {
-   
-    ele.onclick= function () {
-        items.forEach(function(ele) {
-            ele.classList.remove('active')
-        })
-        ele.classList.add('active')
-    }
-})
+items.forEach(function (ele) {
+  ele.onclick = function () {
+    items.forEach(function (ele) {
+      ele.classList.remove("active");
+    });
+    this.classList.add("active");
+  };
+});
+// set switch mode
+let contentMode = document.querySelector(".switch-mode div");
+let btnSwitchMode = document.querySelector(".switch-mode div .btn");
+
+//  1 way
+btnSwitchMode.onclick = function () {
+  btnSwitchMode.classList.toggle('sun-mode-btn')
+  contentMode.classList.toggle('sun-mode-content-btn')
+};
+
